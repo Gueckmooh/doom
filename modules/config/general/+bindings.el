07805affbb412f35ebf5697060be28ce3c636370
gueckmooh <gueckmooh@pm.me>
2023-04-17 00:55:07 +0200

    Fix typo

--- modules/config/general/+bindings.el
+++ modules/config/general/+bindings.el
@@ -6,5 +6,5 @@
 (map! :leader
      :desc "Mark sexp" "a" #'mark-sexp
      (:prefix-map ("r" . "replace")
-      :desc "Query replace" "r" #'anzy-query-replace
+      :desc "Query replace" "r" #'anzu-query-replace
       :desc "Query replace regexp" "R" #'anzu-query-replace-regexp))
