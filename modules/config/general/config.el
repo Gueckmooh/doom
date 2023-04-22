1db344b9549bcf5625d19c50f8ff6301f7e4c99e
gueckmooh <gueckmooh@pm.me>
2023-04-22 19:42:55 +0200

    Ignore .ccls-cache files in projectile

--- modules/config/general/config.el
+++ modules/config/general/config.el
@@ -1,5 +1,9 @@
 ;;; config/general/config.el -*- lexical-binding: t; -*-
 
+(after! projectile
+  (add-to-list 'projectile-globally-ignored-directories "*.ccls-cache"))
+
+
 (when (modulep! +lsp)
   (load! "+lsp"))
 
