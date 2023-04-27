73a3698bc3302989be6498de449a6ae3bc4fa1ed
gueckmooh <gueckmooh@pm.me>
2023-04-28 00:20:46 +0200

    Fix undo-tree messing with evil mode

--- modules/tools/undo-tree/config.el
+++ modules/tools/undo-tree/config.el
@@ -3,6 +3,7 @@
 (use-package! undo-tree
   :config
   (setq-default undo-tree-history-directory-alist '(("." . "~/.config/emacs/undo")))
+  (setq-default undo-tree-enable-undo-in-region nil)
   (global-undo-tree-mode t))
 
 (map! :leader "U" #'undo-tree-visualize)
